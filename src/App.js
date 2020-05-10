692a7fe02cd852261413b451dba8fd1e5e45adc0
Laurentiu <gotryit.coding@gmail.com>
2020-05-10 20:41:41 +0300

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [jwtToken, setJwtToken] = useState('')
+
+  const authenticateUser = async () => {
+    var requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json, text/plain, */*'
+      },
+      body: JSON.stringify({ username: username, password: password })
+    };
+    
+    let response = await fetch("https://gotryit-api.herokuapp.com/api/weather/authenticate", requestOptions)
+
+    let { token, expireDate } = await response.json()
+
+    setJwtToken(token)
+
+    console.log(token)
+    console.log(expireDate)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {jwtToken.length === 0 ?
+      <React.Fragment>
+        <div className="user-input">
+          Username: <input onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="user-input">
+          Password: <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button onClick={() => authenticateUser()}>Authenticate</button>
+      </React.Fragment> :
+      <React.Fragment>
+          <button onClick={() => setJwtToken('')}>Sign Out</button>
+      </React.Fragment>}
     </div>
   );
 }
